ca27dfac672a1ebe9a17ebd7c4e9ef1c8c65cbad
rdellacorna <riccardo.dc.marconi@gmail.com>
2022-04-14 16:08:42 +0200

    Making of an Animated Splash Screen

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.composecourseyt
 
 import android.os.Bundle
+import android.view.animation.OvershootInterpolator
+import android.window.SplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -14,11 +18,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -26,97 +26,79 @@ import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.delay
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Surface(
-                color = Color(0xFF101010),
-                modifier = Modifier.fillMaxSize()
-            ) {
-                DropDown(
-                    text = "Test",
-
-                    modifier = Modifier.padding(15.dp)
-                ) {
-                    Text(
-                        text = "Good morning!", modifier = Modifier
-                            .fillMaxWidth()
-                            .height(100.dp)
-                            .background(Color.Green)
-                            .padding(20.dp)
-                    )
-                }
+            Surface(color = Color(0xff202020), modifier = Modifier.fillMaxSize()) {
+                Navigation()
             }
         }
     }
 }
 
 @Composable
-fun DropDown(
-    text: String,
-    modifier: Modifier = Modifier,
-    initiallyOpened: Boolean = false,
-    content: @Composable () -> Unit
-) {
-    var isOpen by remember {
-        mutableStateOf(initiallyOpened)
+fun Navigation(){
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "splash_screen"
+    ){
+        composable("splash_screen"){
+            SplashScreen(navController = navController)
+        }
+        composable("main_screen"){
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "MAIN SCREEN",
+                    color = Color.White
+                )
+            }
+        }
     }
-    val alpha = animateFloatAsState(
-        targetValue = if(isOpen) 1f else 0f,
-        animationSpec = tween(
-            durationMillis = 300
-        )
-    )
-    val rotateX = animateFloatAsState(
-        targetValue = if(isOpen) 0f else -90f,
-        animationSpec = tween(
-            durationMillis = 200
-        )
-    )
-    Column(
-        modifier = modifier
-            .fillMaxWidth()
-    ) {
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            Text(
-                text = text,
-                color = Color.White,
-                fontSize = 16.sp
-            )
-            Icon(
-                imageVector = Icons.Default.ArrowDropDown,
-                contentDescription = "Open or close the drop down",
-                tint = Color.White,
-                modifier = Modifier
-                    .clickable {
-                        isOpen = !isOpen
-                    }
-                    .scale(1f, if (isOpen) -1f else 1f)
-            )
-        }
-        Spacer(modifier = Modifier.height(10.dp))
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier
-                .fillMaxWidth()
-                .graphicsLayer {
-                    transformOrigin = TransformOrigin(0.5f, 0f)
-                    rotationX = rotateX.value
+}
+
+@Composable
+fun SplashScreen(navController: NavController){
+    val scale = remember{
+        Animatable(0f)
+    }
+    LaunchedEffect(key1 = true){
+        scale.animateTo(
+            targetValue = 0.3f,
+            animationSpec = tween(
+                durationMillis = 1500,
+                easing = {
+                    OvershootInterpolator(6f).getInterpolation(it)
                 }
-                .alpha(alpha.value)
-        ) {
-            content()
-        }
+            )
+        )
+        delay(300L)
+        navController.navigate("main_screen")
+    }
+    Box(
+        contentAlignment = Alignment.Center, 
+        modifier = Modifier.fillMaxSize()
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.photo),
+            contentDescription = "Logo",
+            modifier = Modifier.scale(scale.value),
+
+        )
     }
 }
