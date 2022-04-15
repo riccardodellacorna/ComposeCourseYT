cd893f3a7906a59b0269b519c84d8083cb077fbc
rdellacorna <riccardo.dc.marconi@gmail.com>
2022-04-15 12:13:05 +0200

    Clean Theming in Compose

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,79 +34,24 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import com.example.composecourseyt.ui.theme.ComposeCourseYTTheme
+import com.example.composecourseyt.ui.theme.LocalSpacing
+import com.example.composecourseyt.ui.theme.spacing
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import java.util.jar.Manifest
 
-@OptIn(ExperimentalPermissionsApi::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeCourseYTTheme {
-                val permissionsStase = rememberMultiplePermissionsState(
-                    permissions =  listOf(
-                        android.Manifest.permission.RECORD_AUDIO,
-                        android.Manifest.permission.CAMERA,
+                Surface(
+                    color = MaterialTheme.colors.background,
+                    modifier = Modifier.padding(
+                        MaterialTheme.spacing.medium
                     )
-                )
-                val lifecycleOwner = LocalLifecycleOwner.current
-                DisposableEffect(
-                    key1 = lifecycleOwner,
-                    effect = {
-                        val observer = LifecycleEventObserver { _, event ->
-                            if(event == Lifecycle.Event.ON_START){
-                                permissionsStase.launchMultiplePermissionRequest()
-                            }
-                        }
-                        lifecycleOwner.lifecycle.addObserver(observer)
-
-                        onDispose {
-                            lifecycleOwner.lifecycle.removeObserver(observer)
-                        }
-                    }
-                )
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    permissionsStase.permissions.forEach{ perm ->
-                        when(perm.permission){
-                            android.Manifest.permission.CAMERA -> {
-                                when {
-                                    perm.hasPermission -> {
-                                        Text(text = "Camera permission ACCEPTED")
-                                    }
-                                    perm.shouldShowRationale -> {
-                                        Text(text = "Camera permission is needed"
-                                                + "to access the camera")
-                                    }
-                                    perm.isPermanentlyDenied() -> {
-                                        Text(text = "Camera permission was permanently"
-                                                + "denied. You can enable it in the app"
-                                                + "settings.")
-                                    }
-                                }
-                            }
-                            android.Manifest.permission.RECORD_AUDIO -> {
-                                when {
-                                    perm.hasPermission -> {
-                                        Text(text = "Record audio permission ACCEPTED")
-                                    }
-                                    perm.shouldShowRationale -> {
-                                        Text(text = "Record audio permission is needed"
-                                                + "to access the camera")
-                                    }
-                                    perm.isPermanentlyDenied() -> {
-                                        Text(text = "Record audio permission was permanently"
-                                                + "denied. You can enable it in the app"
-                                                + "settings.")
-                                    }
-                                }
-                            }
-                        }
-                    }
+                ){
+                    LocalSpacing.current.medium
                 }
             }
         }
