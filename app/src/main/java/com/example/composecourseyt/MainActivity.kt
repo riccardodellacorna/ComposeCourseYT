02aa0f7e74344d0887d89801a9ed79fc07e832ee
rdellacorna <riccardo.dc.marconi@gmail.com>
2022-04-15 12:37:37 +0200

    Compose Navigation

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -1,59 +1,147 @@
 package com.example.composecourseyt
 
-import android.content.res.Resources
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
-import androidx.compose.ui.input.nestedscroll.NestedScrollSource
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.composecourseyt.ui.theme.ComposeCourseYTTheme
-import com.example.composecourseyt.ui.theme.LocalSpacing
-import com.example.composecourseyt.ui.theme.spacing
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.rememberMultiplePermissionsState
-import java.util.jar.Manifest
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeCourseYTTheme {
-                Surface(
-                    color = MaterialTheme.colors.background,
-                    modifier = Modifier.padding(
-                        MaterialTheme.spacing.medium
-                    )
-                ){
-                    LocalSpacing.current.medium
+                val navController = rememberNavController() //utilizzato per "navigare" tra schermate differenti
+                NavHost(
+                    navController = navController,
+                    startDestination = "login"
+                ) {
+                    composable("login") {
+                        LoginScreen(navController)
+                    }
+                    composable(
+                        route = "profile/{name}/{userId}/{timestamp}",
+                        arguments = listOf(
+                            navArgument("name") {
+                                type = NavType.StringType
+                            },
+                            navArgument("userId") {
+                                type = NavType.StringType
+                            },
+                            navArgument("timestamp") {
+                                type = NavType.LongType
+                            },
+                        )
+                    ) {
+                        val name = it.arguments?.getString("name")!!
+                        val userId = it.arguments?.getString("userId")!!
+                        val timestamp = it.arguments?.getLong("timestamp")!!
+
+                        ProfileScreen(
+                            navController = navController,
+                            name = name,
+                            userId = userId,
+                            created = timestamp
+                        )
+                    }
+                    composable("post/{showOnlyPostsByUser}", arguments = listOf(
+                        navArgument("showOnlyPostsByUser") {
+                            type = NavType.BoolType
+                            defaultValue = false
+                        }
+                    )) {
+                        val showOnlyPostsByUser =
+                            it.arguments?.getBoolean("showOnlyPostsByUser") ?: false
+                        PostScreen(showOnlyPostsByUser)
+                    }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun LoginScreen(
+    navController: NavController
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Login Screen")
+        Button(onClick = {
+            navController.navigate("profile/philipp/userid/123456789")
+        }) {
+            Text("Go to Profile Screen")
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun ProfileScreen(
+    navController: NavController,
+    name: String,
+    userId: String,
+    created: Long
+) {
+    val user = remember {
+        User(
+            name = name,
+            id = userId,
+            created = LocalDateTime.ofInstant(
+                Instant.ofEpochMilli(created), ZoneId.systemDefault()
+            )
+        )
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Profile Screen: $user", textAlign = TextAlign.Center)
+        Button(onClick = {
+            navController.navigate("post/true")
+        }) {
+            Text("Go to Post Screen")
+        }
+    }
+}
+
+@Composable
+fun PostScreen(
+    showOnlyPostsByUser: Boolean = false
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Post Screen, $showOnlyPostsByUser")
     }
 }
