cdc820f50615b2ae900ee162e17da46111c04df0
rdellacorna <riccardo.dc.marconi@gmail.com>
2022-04-15 11:17:12 +0200

    How to Do Permission Handling

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.MaterialTheme
@@ -28,120 +25,89 @@ import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import com.example.composecourseyt.ui.theme.ComposeCourseYTTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import java.util.jar.Manifest
 
+@OptIn(ExperimentalPermissionsApi::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeCourseYTTheme {
-                val moonScrollSpeed = 0.08f
-                val midBgScrollSpeed = 0.03f
-
-                val imageHeight = (LocalConfiguration.current.screenWidthDp * (2f / 3f)).dp
-                val lazyListState = rememberLazyListState()
-
-                var moonOffset by remember {
-                    mutableStateOf(0f)
-                }
-                var midBgOffset by remember {
-                    mutableStateOf(0f)
-                }
-
-                //.. to create a parallax scrolling effect:
-                val nestedScrollConnection = object : NestedScrollConnection {
-                    override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
-                        val delta = available.y
-                        val layoutInfo = lazyListState.layoutInfo
-                        // Check if the first item is visible
-                        if(lazyListState.firstVisibleItemIndex == 0) {
-                            return Offset.Zero
+                val permissionsStase = rememberMultiplePermissionsState(
+                    permissions =  listOf(
+                        android.Manifest.permission.RECORD_AUDIO,
+                        android.Manifest.permission.CAMERA,
+                    )
+                )
+                val lifecycleOwner = LocalLifecycleOwner.current
+                DisposableEffect(
+                    key1 = lifecycleOwner,
+                    effect = {
+                        val observer = LifecycleEventObserver { _, event ->
+                            if(event == Lifecycle.Event.ON_START){
+                                permissionsStase.launchMultiplePermissionRequest()
+                            }
                         }
-                        // Check other items
-                        if(layoutInfo.visibleItemsInfo.lastOrNull()?.index == layoutInfo.totalItemsCount - 1) {
-                            return Offset.Zero
+                        lifecycleOwner.lifecycle.addObserver(observer)
+
+                        onDispose {
+                            lifecycleOwner.lifecycle.removeObserver(observer)
                         }
-                        moonOffset += delta * moonScrollSpeed
-                        midBgOffset += delta * midBgScrollSpeed
-                        return Offset.Zero
                     }
-                }
-                LazyColumn(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .nestedScroll(nestedScrollConnection),
-                    state = lazyListState
+                )
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
                 ) {
-                    items(10) {
-                        Text(
-                            text = "Sample item",
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(16.dp)
-                        )
-                    }
-                    item {
-                        Box(
-                            modifier = Modifier
-                                .clipToBounds()
-                                .fillMaxWidth()
-                                .height(imageHeight + midBgOffset.toDp())
-                                .background(
-                                    Brush.verticalGradient(
-                                        listOf(
-                                            Color(0xFFf36b21),
-                                            Color(0xFFf9a521)
-                                        )
-                                    )
-                                )
-                        ) {
-                            Image(
-                                painter = painterResource(id = R.drawable.ic_moonbg),
-                                contentDescription = "moon",
-                                contentScale = ContentScale.FillWidth,
-                                alignment = Alignment.BottomCenter,
-                                modifier = Modifier.matchParentSize()
-                                    .graphicsLayer {   //utile per applicare effetti all' immagine
-                                        translationY = moonOffset
+                    permissionsStase.permissions.forEach{ perm ->
+                        when(perm.permission){
+                            android.Manifest.permission.CAMERA -> {
+                                when {
+                                    perm.hasPermission -> {
+                                        Text(text = "Camera permission ACCEPTED")
                                     }
-                            )
-                            Image(
-                                painter = painterResource(id = R.drawable.ic_midbg),
-                                contentDescription = "mid bg",
-                                contentScale = ContentScale.FillWidth,
-                                alignment = Alignment.BottomCenter,
-                                modifier = Modifier.matchParentSize()
-                                    .graphicsLayer {
-                                        translationY = midBgOffset
+                                    perm.shouldShowRationale -> {
+                                        Text(text = "Camera permission is needed"
+                                                + "to access the camera")
                                     }
-                            )
-                            Image(
-                                painter = painterResource(id = R.drawable.ic_outerbg),
-                                contentDescription = "outer bg",
-                                contentScale = ContentScale.FillWidth,
-                                alignment = Alignment.BottomCenter,
-                                modifier = Modifier.matchParentSize()
-                            )
+                                    perm.isPermanentlyDenied() -> {
+                                        Text(text = "Camera permission was permanently"
+                                                + "denied. You can enable it in the app"
+                                                + "settings.")
+                                    }
+                                }
+                            }
+                            android.Manifest.permission.RECORD_AUDIO -> {
+                                when {
+                                    perm.hasPermission -> {
+                                        Text(text = "Record audio permission ACCEPTED")
+                                    }
+                                    perm.shouldShowRationale -> {
+                                        Text(text = "Record audio permission is needed"
+                                                + "to access the camera")
+                                    }
+                                    perm.isPermanentlyDenied() -> {
+                                        Text(text = "Record audio permission was permanently"
+                                                + "denied. You can enable it in the app"
+                                                + "settings.")
+                                    }
+                                }
+                            }
                         }
-                    }
-                    items(20) {
-                        Text(
-                            text = "Sample item",
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(16.dp)
-                        )
                     }
                 }
             }
         }
-    }
-
-    private fun Float.toDp(): Dp {
-        return (this / Resources.getSystem().displayMetrics.density).dp
     }
 }
